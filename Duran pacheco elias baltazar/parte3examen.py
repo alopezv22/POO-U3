dcd048cbee9d8ff21b76675fc01986f45e6b6653
ELIASDURAN12 <150065758+ELIASDURAN12@users.noreply.github.com>
2023-11-06 12:38:02 -0600

    Add files via upload

---
+++ Duran pacheco elias baltazar/parte3examen.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+sobresalientes = int(input("Número de personas con calificación sobresaliente : "))
+notable = int(input("Número de personas con calificación notable : "))
+adecuado = int(input("Número de personas con calificación adecuado : "))
+insuficiente = int(input("Número de personas con calificación insuficiente : "))
+
+personas = sobresalientes + notable + adecuado + insuficiente
+psobresalientes = sobresalientes / personas * 100
+pnotable = notable / personas * 100
+padecuado = adecuado / personas * 100
+pinsuficiente = insuficiente / personas * 100
+
+etiquetas = ['Sobresaliente', 'Notable', 'Adecuado', 'Insuficiente']
+
+porcentajes = [psobresalientes, pnotable, padecuado, pinsuficiente]
+
+plt.bar(etiquetas, porcentajes)
+plt.title('Distribución de Calificaciones')
+plt.xlabel('Calificación')
+plt.ylabel('Porcentaje (%)')
+
+plt.show()
