b5adf59071f32f98c65a3e556160c2c2155e799a
alejandroqm144 <149653645+alejandroqm144@users.noreply.github.com>
2023-11-06 12:39:06 -0600

    Add files via upload

---
+++ Quijano May Alejandro/ExamenEjercicio3.py
@@ -0,0 +1,24 @@
+
+
+import matplotlib.pyplot as plt
+
+def crear_grafico_barras(calificaciones):
+    calificacion_labels = ["A", "B", "C", "D"]
+    plt.bar(calificacion_labels, calificaciones)
+    plt.xlabel("Calificaciones")
+    plt.ylabel("Número de personas")
+    plt.title("Gráfico de Barras de Calificaciones")
+    plt.show()
+
+def main():
+    try:
+        calificaciones = []
+        for i in range(4):
+            calificacion = int(input(f"Introduce el número de personas con calificación {chr(65 + i)}: "))
+            calificaciones.append(calificacion)
+
+        crear_grafico_barras(calificaciones)
+    except ValueError:
+        print("Debes introducir valores numéricos válidos para el número de personas.")
+
+main()
